a5590051c4bf1d36ed684ab6110d2ce410c4bd52
TianRy1337 <Ryan55014TianZhang@gmail.com>
2020-10-28 15:53:48 +0800

    43_PB

---
+++ Assets/Rocket.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            print("Thrusting");
+        } 
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            print("Rotating Left");
+        }else if (Input.GetKeyDown(KeyCode.D))
+        {
+            print("Rotating Right");
+        }
+    }
+}
