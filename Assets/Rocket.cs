aa1cc4e65c5b3230332bd2b4ba83f2bb4bd49da7
TianRy1337 <Ryan55014TianZhang@gmail.com>
2020-10-28 16:33:27 +0800

    44_PB

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigiBody;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigiBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -20,7 +22,7 @@ public class Rocket : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            print("Thrusting");
+            rigiBody.AddRelativeForce(Vector3.up);
         } 
         if (Input.GetKeyDown(KeyCode.A))
         {
